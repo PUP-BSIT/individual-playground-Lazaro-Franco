a9e20cd405acce1df938871778dcaa6f4b684819
Lazaro-Franco <francoalfonso411@gmail.com>
2024-10-11 21:59:02 +0800

    Add more alternative paths

---
+++ Testdashone.py
@@ -0,0 +1,14 @@
+print("Hello! Welcome to decision making!")
+print("here the situation: is theres a fire at a random house and theres people inside")
+print("you a). rescue them and call 911, b). just steal all of there valuables c).join them")
+
+N1 = input("Enter your Choice[]:")
+if N1 == "a":
+    print("You chose to be a hero!")
+    print("you got the best ending!")
+elif N1 == "b":
+    print("You chose to be a heartless thief!")
+    print("you got the heartless ending")
+else:
+    print("you chose to be a mad man!")
+    print("you got the Insane ending....")
